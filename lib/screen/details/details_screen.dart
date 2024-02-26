3473fc52e06640e9d66ebea112ef81852d633477
R80 <gohilsumit142@gmail.com>
2024-02-26 16:48:40 +0530

    Uri

--- lib/screen/details/details_screen.dart
+++ lib/screen/details/details_screen.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../../model/contact_model.dart';
 
@@ -30,7 +32,6 @@ class _DetailsScreenState extends State<DetailsScreen> {
             child: Column(
               children: [
                 Container(
-                  color: Colors.grey.shade50,
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     mainAxisSize: MainAxisSize.min,
@@ -38,8 +39,8 @@ class _DetailsScreenState extends State<DetailsScreen> {
                       Container(
                         child: c2?.image=="assets/image/profile.png"
                             ?
-                        Center(
-                          child: const CircleAvatar(
+                        const Center(
+                          child: CircleAvatar(
                             radius: 60,
                             backgroundImage: AssetImage("assets/image/profile.png"),
                           ),
@@ -54,53 +55,62 @@ class _DetailsScreenState extends State<DetailsScreen> {
                       Center(
                         child: Text(
                           "${c2?.name}",
-                          style: TextStyle(
+                          style: const TextStyle(
                               fontWeight: FontWeight.bold, fontSize: 21),
                         ),
                       ),
-                      SizedBox(height: 15,),
+                      const SizedBox(height: 15,),
                     ],
                   ),
                 ),
-                SizedBox(height: 15,),
-                const Row(
+                const SizedBox(height: 15,),
+                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Column(
                       children: [
-                        CircleAvatar(child: Icon(Icons.call,size: 25,color: Colors.white,),backgroundColor: Colors.green,radius: 20,),
-                        Text("call",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),)
+                        InkWell(
+                          onTap: () async{
+                            Uri mail= Uri.parse("mailto :${c2!.email}");
+                            await launchUrl(mail);
+                          },
+                            child: CircleAvatar(child: Icon(Icons.mail,size: 25,color: Colors.white,),backgroundColor: Colors.blue,radius: 20,)),
+                        Text("email",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),)
+                      ],
+                    ),
+                    const Column(
+                      children: [
+                        CircleAvatar(child: Icon(Icons.message,size: 25,color: Colors.white,),backgroundColor: Colors.red,radius: 20,),
+                        Text("Video Call",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),)
                       ],
                     ),
                     Column(
                       children: [
-                        CircleAvatar(child: Icon(Icons.message,size: 25,color: Colors.white,),backgroundColor: Colors.blue,radius: 20,),
-                        Text("Text",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),)
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        CircleAvatar(child: Icon(Icons.video_call,size: 25,color: Colors.white,),backgroundColor: Colors.red,radius: 20,),
-                        Text("video",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),)
+                        InkWell(onTap: () async {
+                          Uri call= Uri.parse("tel : +91${c2!.mobile}");
+                          await launchUrl(call);
+                        },
+                            child: const CircleAvatar(child: Icon(Icons.call,size: 25,color: Colors.white,),backgroundColor: Colors.green,radius: 20,)),
+                        const Text("Call",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),)
                       ],
                     ),
                   ],
                 ),
-                SizedBox(height: 15,),
-                Align(
+                const SizedBox(height: 15,),
+                const Align(
                   alignment: AlignmentDirectional.topStart,
                     child: Text("NOW",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),)),
                 Row(children: [
                   Column(
                     children: [
-                      Text("${c2?.mobile}",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18),),
-                      Text("${c2?.email}",style: TextStyle(fontSize: 15),),
+                      Text("${c2?.mobile}",style: const TextStyle(fontWeight: FontWeight.bold,fontSize: 18),),
+                      Text("${c2?.email}",style: const TextStyle(fontSize: 15),),
                     ],
                   ),
-                  Spacer(),
-                  Icon(Icons.mail,size: 30,),
-                  SizedBox(width: 15,),
-                  Icon(Icons.call,size: 30,),
+                  const Spacer(),
+                  const Icon(Icons.mail,size: 30,),
+                  const SizedBox(width: 15,),
+                  const Icon(Icons.call,size: 30,),
 
                 ],)
               ],
